ea9aa54143479ae46943f78116b323a3dd0b5c48
regisPatrick <regis86@hotmail.com>
2020-03-26 00:02:00 -0300

    commit 26-03-2020

--- Cursos/src/main/java/com/regisprojects/Cursos/repository/EventoRepository.java
+++ Cursos/src/main/java/com/regisprojects/Cursos/repository/EventoRepository.java
@@ -14,6 +14,6 @@ import org.springframework.data.repository.CrudRepository;
  */
 public interface EventoRepository extends CrudRepository<Evento, String>{
     
-    
+    Evento findByCodigo(long codigo);
     
 }
