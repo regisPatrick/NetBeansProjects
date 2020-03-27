dd7cd4409e0f2832e9e73e544eb1b048369ed811
regisPatrick <regis86@hotmail.com>
2020-03-26 23:54:36 -0300

    commit 26-03-2020

--- Cursos/src/main/java/com/regisprojects/Cursos/models/Evento.java
+++ Cursos/src/main/java/com/regisprojects/Cursos/models/Evento.java
@@ -6,10 +6,12 @@
 package com.regisprojects.Cursos.models;
 
 import java.io.Serializable;
+import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 /**
  *
@@ -31,7 +33,10 @@ public class Evento implements Serializable{
     private String data;
     
     private String horario;
-
+    
+    @OneToMany
+    private List<Convidado> convidados;
+    
     public long getCodigo() {
         return codigo;
     }
