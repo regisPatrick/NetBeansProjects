fc085ec4622f8d2661ba7d2d5c2c75befef50df2
regisPatrick <regis86@hotmail.com>
2020-04-01 00:08:41 -0300

    commit 31-03-2020

--- Cursos/src/main/java/com/regisprojects/Cursos/controllers/EventoController.java
+++ Cursos/src/main/java/com/regisprojects/Cursos/controllers/EventoController.java
@@ -68,6 +68,13 @@ public class EventoController {
         return mv;
     }
     
+    @RequestMapping("/deletarEvento")
+    public String deletarEvento(long codigo){
+        Evento evento = er.findByCodigo(codigo);
+        er.delete(evento);
+        return "redirect:/eventos";
+    }
+    
     @RequestMapping(value="/{codigo}", method=RequestMethod.POST)
     public String detalhesEventoPost(@PathVariable("codigo") long codigo, @Valid Convidado convidado, BindingResult result, RedirectAttributes attributes){
         if(result.hasErrors()){
@@ -79,6 +86,17 @@ public class EventoController {
         cr.save(convidado);
         attributes.addFlashAttribute("mensagem", "Convidado adicionado com sucesso!");
         return "redirect:/{codigo}";
+    }
+    
+    @RequestMapping("/deletarConvidado")
+    public String deletarConvidado(String rg){
+        Convidado convidado = cr.findByRg(rg);
+        cr.delete(convidado);
+        
+        Evento evento = convidado.getEvento();
+        long codigoLong = evento.getCodigo();
+        String codigo = "" + codigoLong;
+        return "redirect:/" + codigo;
     }
     
 }
