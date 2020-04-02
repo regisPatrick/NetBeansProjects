df6654c1787ecf751f9d4338ec46b97ec273466f
regisPatrick <regis86@hotmail.com>
2020-04-01 22:22:05 -0300

    commit 01-04-2020

---
+++ APIRestSpringBootProdutos/src/main/java/com/regisprojects/APIRestSpringBootProdutos/ApiRestSpringBootProdutosApplication.java
@@ -0,0 +1,13 @@
+package com.regisprojects.APIRestSpringBootProdutos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestSpringBootProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestSpringBootProdutosApplication.class, args);
+	}
+
+}
