893340e20791c9cfdba234462363e4430aedac9a
regisPatrick <regis86@hotmail.com>
2020-04-04 01:57:27 -0300

    commit 03-04-2020

---
+++ APIRestSpringBootProdutos/src/main/java/com/regisprojects/APIRestSpringBootProdutos/resources/ProdutoResource.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.regisprojects.APIRestSpringBootProdutos.resources;
+
+import com.regisprojects.APIRestSpringBootProdutos.models.Produto;
+import com.regisprojects.APIRestSpringBootProdutos.repository.ProdutoRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author user
+ */
+@RestController
+@RequestMapping(value = "/api")
+public class ProdutoResource {
+    
+    @Autowired
+    ProdutoRepository produtoRepository;
+    
+    @GetMapping("/produtos")
+    public List<Produto> listaProdutos(){
+        return produtoRepository.findAll();
+    }
+    
+    @GetMapping("/produto/{id}")
+    public Produto listaProdutoUnico(@PathVariable(value="id") long id){
+        return produtoRepository.findById(id);
+    }
+    
+    @PostMapping("/produto")
+    public Produto salvaProduto(@RequestBody Produto produto){
+        return produtoRepository.save(produto);
+    }
+    
+}
