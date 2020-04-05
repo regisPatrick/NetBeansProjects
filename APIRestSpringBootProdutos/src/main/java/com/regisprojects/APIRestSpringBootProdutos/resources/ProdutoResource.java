75edfcc93ee565c62a05f17d968a6da7afc02ae7
regisPatrick <regis86@hotmail.com>
2020-04-05 01:51:33 -0300

    commit 04-04-2020

--- APIRestSpringBootProdutos/src/main/java/com/regisprojects/APIRestSpringBootProdutos/resources/ProdutoResource.java
+++ APIRestSpringBootProdutos/src/main/java/com/regisprojects/APIRestSpringBootProdutos/resources/ProdutoResource.java
@@ -9,9 +9,11 @@ import com.regisprojects.APIRestSpringBootProdutos.models.Produto;
 import com.regisprojects.APIRestSpringBootProdutos.repository.ProdutoRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,6 +41,16 @@ public class ProdutoResource {
     
     @PostMapping("/produto")
     public Produto salvaProduto(@RequestBody Produto produto){
+        return produtoRepository.save(produto);
+    }
+    
+    @DeleteMapping("/produto")
+    public void deleteProduto(@RequestBody Produto produto){
+        produtoRepository.delete(produto);
+    }
+    
+    @PutMapping("/produto")
+    public Produto atualizaProduto(@RequestBody Produto produto){
         return produtoRepository.save(produto);
     }
     
