aa7fa4c1a4ade4b7ed54849e540dff8f5c81c53c
regisPatrick <regis86@hotmail.com>
2020-04-15 03:49:34 -0300

    commit 15-04-2020

---
+++ myblog/src/main/java/com/regisprojects/myblog/myblog/controller/CodeblogController.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.regisprojects.myblog.myblog.controller;
+
+import com.regisprojects.myblog.myblog.model.Post;
+import com.regisprojects.myblog.myblog.service.CodeblogService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * @author user
+ */
+@Controller
+public class CodeblogController {
+    
+    @Autowired
+    CodeblogService codeblogService;
+    
+    @RequestMapping(value = "/posts", method = RequestMethod.GET)
+    public ModelAndView getPosts(){
+        ModelAndView mv = new ModelAndView("posts");
+        List<Post> posts = codeblogService.findAll();
+        mv.addObject("posts", posts);
+        return mv;
+    }
+    
+}
