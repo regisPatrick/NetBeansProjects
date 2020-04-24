f48e9f327c5c3cbc4f0b70e1a3aac31363575ab5
regisPatrick <regis86@hotmail.com>
2020-04-24 01:45:00 -0300

    commit 24-04-2020

---
+++ webfluxspringboot/src/main/java/com/apirest/webfluxspringboot/WebfluxspringbootApplication.java
@@ -0,0 +1,14 @@
+package com.apirest.webfluxspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxspringbootApplication.class, args);
+                System.out.println("Ola mundo com webflux!!!");
+	}
+
+}
---
+++ webfluxspringboot/src/test/java/com/apirest/webfluxspringboot/WebfluxspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apirest.webfluxspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
