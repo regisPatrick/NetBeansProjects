ec4db7e2d47e0ea8f42e45bfcc36d2cb693916c3
regisPatrick <regis86@hotmail.com>
2020-04-28 22:31:08 -0300

    commit 28-04-2020 2

---
+++ webfluxspringboot/src/main/java/com/apirest/webfluxspringboot/PlaylistHandler.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.apirest.webfluxspringboot;
+
+import com.apirest.webfluxspringboot.document.Playlist;
+import com.apirest.webfluxspringboot.services.PlaylistService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+import static org.springframework.web.reactive.function.BodyInserters.fromPublisher;
+
+
+/**
+ *
+ * @author user
+ */
+@Component
+public class PlaylistHandler {
+    
+    @Autowired
+    PlaylistService service;
+    
+    public Mono<ServerResponse> findAll(ServerRequest request){
+        return ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(service.findAll(), Playlist.class);
+    }
+    
+    public Mono<ServerResponse> findById(ServerRequest request){
+        String id = request.pathVariable("id");
+        return ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(service.findById(id), Playlist.class);
+    }
+    
+    public Mono<ServerResponse> save(ServerRequest request){
+        final Mono<Playlist> playlist = request.bodyToMono(Playlist.class);
+        return ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(fromPublisher(playlist.flatMap(service::save), Playlist.class));
+    }
+    
+}
