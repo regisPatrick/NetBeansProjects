2243aea23cce32573d992e7136ddd73b562b3d8e
regisPatrick <regis86@hotmail.com>
2020-05-08 03:10:23 -0300

    commit 08-05-2020

--- SpringBootProjectTest/src/main/java/br/com/regisprojects/springbootprojecttest/awesome/ApplicationStart.java
+++ SpringBootProjectTest/src/main/java/br/com/regisprojects/springbootprojecttest/awesome/ApplicationStart.java
@@ -3,18 +3,22 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.com.regisprojects.springbootprojecttest.awesome.start;
+package br.com.regisprojects.springbootprojecttest.awesome;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 /**
  *
  * @author user
  */
-@EnableAutoConfiguration
-@ComponentScan(basePackages = "br.com.regisprojects.springbootprojecttest.awesome.endpoint")
+//@EnableAutoConfiguration
+//@ComponentScan// (basePackages = "br.com.regisprojects.springbootprojecttest.awesome.endpoint")
+//@Configuration
+@SpringBootApplication // @Configuration @EnableAutoConfiguration @ComponentScan
 public class ApplicationStart {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationStart.class, args);
