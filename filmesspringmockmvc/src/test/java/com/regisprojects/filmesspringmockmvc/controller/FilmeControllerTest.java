cd66a0f4d4ba7f39c884668878f78eaf28208697
regisPatrick <regis86@hotmail.com>
2020-05-11 03:01:38 -0300

    commit 11-05-2020

--- filmesspringmockmvc/src/test/java/com/regisprojects/filmesspringmockmvc/controller/FilmeControllerTest.java
+++ filmesspringmockmvc/src/test/java/com/regisprojects/filmesspringmockmvc/controller/FilmeControllerTest.java
@@ -5,11 +5,17 @@
  */
 package com.regisprojects.filmesspringmockmvc.controller;
 
-import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import com.regisprojects.filmesspringmockmvc.model.Filme;
+import com.regisprojects.filmesspringmockmvc.service.FilmeService;
+import io.restassured.http.ContentType;
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.when;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 
 /**
  *
@@ -21,13 +27,25 @@ public class FilmeControllerTest {
     @Autowired
     private FilmeController filmeController;
     
+    @MockBean
+    private FilmeService filmeService;
+    
     @BeforeEach
     public void setup(){
-        RestAssuredMockMvc.standaloneSetup(this.filmeController);
+        standaloneSetup(this.filmeController);
     }
     
     @Test
     public void deveRetornarSucesso_QuandoBuscarFilme(){
-        RestAssuredMockMvc.given();
+        
+        when(this.filmeService.obterFilme(1L))
+            .thenReturn(new Filme(1L, "O Poderoso Chefão", "Sem descrição"));
+        
+        given()
+            .accept(ContentType.JSON)
+        .when()
+            .get("/filmes/{codigo}", 1L)
+        .then()
+            .statusCode(HttpStatus.OK.value());
     }
 }
