e04dbf27dc5d15af8be6f7e4242192e5ee55bcc5
regisPatrick <regis86@hotmail.com>
2020-05-16 03:43:35 -0300

    commit 16-05-2020

--- cursospringbootessentials/src/main/java/br/com/regisprojects/cursospringbootessentials/model/Student.java
+++ cursospringbootessentials/src/main/java/br/com/regisprojects/cursospringbootessentials/model/Student.java
@@ -8,39 +8,41 @@ package br.com.regisprojects.cursospringbootessentials.model;
 import java.util.ArrayList;
 import static java.util.Arrays.asList;
 import java.util.List;
+import javax.persistence.Entity;
 
 /**
  *
  * @author user
  */
-public class Student {
+@Entity
+public class Student extends AbstractEntity{
     
-    private int id;
+//    private int id;
     
     private String name;
     
-    public static List<Student> studentList;
+//    public static List<Student> studentList;
     
-    static{
-        studentRepository();
-    }
-
-    public Student(int id, String name) {
-        this(name);
-        this.id = id;
-    }
-    
-    public Student(String name) {
-        this.name = name;
-    }
-
-    public Student() {
-    }
-    
-    private static void studentRepository(){
-        studentList = new ArrayList<>(asList(new Student(1, "Deku"), new Student(2, "Todoroki")));
-    }
-
+//    static{
+//        studentRepository();
+//    }
+//
+//    public Student(int id, String name) {
+//        this(name);
+//        this.id = id;
+//    }
+//    
+//    public Student(String name) {
+//        this.name = name;
+//    }
+//
+//    public Student() {
+//    }
+//    
+//    private static void studentRepository(){
+//        studentList = new ArrayList<>(asList(new Student(1, "Deku"), new Student(2, "Todoroki")));
+//    }
+//
     public String getName() {
         return name;
     }
@@ -48,38 +50,38 @@ public class Student {
     public void setName(String name) {
         this.name = name;
     }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 23 * hash + this.id;
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Student other = (Student) obj;
-        if (this.id != other.id) {
-            return false;
-        }
-        return true;
-    }
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        int hash = 7;
+//        hash = 23 * hash + this.id;
+//        return hash;
+//    }
+//
+//    @Override
+//    public boolean equals(Object obj) {
+//        if (this == obj) {
+//            return true;
+//        }
+//        if (obj == null) {
+//            return false;
+//        }
+//        if (getClass() != obj.getClass()) {
+//            return false;
+//        }
+//        final Student other = (Student) obj;
+//        if (this.id != other.id) {
+//            return false;
+//        }
+//        return true;
+//    }
     
 }
---
+++ cursospringbootessentials/src/main/java/br/com/regisprojects/cursospringbootessentials/repository/StudentRepository.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.regisprojects.cursospringbootessentials.repository;
+
+import br.com.regisprojects.cursospringbootessentials.model.Student;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author user
+ */
+public interface StudentRepository extends CrudRepository<Student, Long>{
+    
+}
