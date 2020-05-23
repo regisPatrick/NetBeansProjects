9d442a22f9c0011effc62b18372dacfb49eb84e0
regisPatrick <regis86@hotmail.com>
2020-05-23 02:52:26 -0300

    commit 23-05-2020

--- cursospringbootessentials/src/main/java/br/com/regisprojects/cursospringbootessentials/handler/RestExceptionHandler.java
+++ cursospringbootessentials/src/main/java/br/com/regisprojects/cursospringbootessentials/handler/RestExceptionHandler.java
@@ -7,9 +7,13 @@ package br.com.regisprojects.cursospringbootessentials.handler;
 
 import br.com.regisprojects.cursospringbootessentials.error.ResourceNotFoundDetails;
 import br.com.regisprojects.cursospringbootessentials.error.ResourceNotFoundException;
+import br.com.regisprojects.cursospringbootessentials.error.ValidationErrorDetails;
 import java.util.Date;
+import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -29,6 +33,20 @@ public class RestExceptionHandler {
                 .title("Resource not found")
                 .detail(rnfException.getMessage())
                 .developerMessage(rnfException.getClass().getName())
+                .build();
+        return new ResponseEntity<>(rnfDetails, HttpStatus.NOT_FOUND);
+    }
+    
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException manvException){
+        List<FieldError> fieldErrors = manvException.getBindingResult().getFieldErrors();
+        ValidationErrorDetails rnfDetails = ValidationErrorDetails.Builder
+                .newBuilder()
+                .timestamp(new Date().getTime())
+                .status(HttpStatus.NOT_FOUND.value())
+                .title("Resource not found")
+                .detail(manvException.getMessage())
+                .developerMessage(manvException.getClass().getName())
                 .build();
         return new ResponseEntity<>(rnfDetails, HttpStatus.NOT_FOUND);
     }
