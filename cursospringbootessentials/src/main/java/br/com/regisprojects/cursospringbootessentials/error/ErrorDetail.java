16cc4e2bca2d0fe04ea928656bdfd2dfbbe5a124
regisPatrick <regis86@hotmail.com>
2020-05-25 00:50:13 -0300

    commit 24-05-2020

--- cursospringbootessentials/src/main/java/br/com/regisprojects/cursospringbootessentials/error/ErrorDetail.java
+++ cursospringbootessentials/src/main/java/br/com/regisprojects/cursospringbootessentials/error/ErrorDetail.java
@@ -57,4 +57,56 @@ public class ErrorDetail {
         this.developerMessage = developerMessage;
     }
     
+    public static final class Builder {
+        private String title;
+        private int status;
+        private String detail;
+        private long timestamp;
+        private String developerMessage;
+        
+        private Builder(){
+        
+        }
+        
+        private static Builder newBuilder(){
+            return new Builder();
+        }
+        
+        public Builder title(String title) {
+            this.title = title;
+            return this;
+        }
+        
+        public Builder status(int status) {
+            this.status = status;
+            return this;
+        }
+        
+        public Builder detail(String detail) {
+            this.detail = detail;
+            return this;
+        }
+        
+        public Builder timestamp(long timestamp) {
+            this.timestamp = timestamp;
+            return this;
+        }
+        
+        public Builder developerMessage(String developerMessage) {
+            this.developerMessage = developerMessage;
+            return this;
+        }
+        
+        public ErrorDetail build(){
+            ErrorDetail errorDetail = new ErrorDetail();
+            errorDetail.setTitle(title);
+            errorDetail.setStatus(status);
+            errorDetail.setDetail(detail);
+            errorDetail.setTimestamp(timestamp);
+            errorDetail.setDeveloperMessage(developerMessage);
+            return errorDetail;
+        }
+        
+    }
+    
 }
--- cursospringbootessentials/src/main/java/br/com/regisprojects/cursospringbootessentials/handler/RestExceptionHandler.java
+++ cursospringbootessentials/src/main/java/br/com/regisprojects/cursospringbootessentials/handler/RestExceptionHandler.java
@@ -17,13 +17,14 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 /**
  *
  * @author user
  */
 @ControllerAdvice
-public class RestExceptionHandler {
+public class RestExceptionHandler extends ResponseEntityExceptionHandler{
     
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException rnfException){
