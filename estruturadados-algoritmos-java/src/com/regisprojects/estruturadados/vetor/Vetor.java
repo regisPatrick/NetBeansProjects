172452250702ac6c4b056c5909649ea5fee162ae
regisPatrick <regis86@hotmail.com>
2020-05-28 01:20:38 -0300

    commit 28-05-2020

---
+++ estruturadados-algoritmos-java/src/com/regisprojects/estruturadados/vetor/Vetor.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.regisprojects.estruturadados.vetor;
+
+/**
+ *
+ * @author user
+ */
+public class Vetor {
+    
+    private String[] elementos;
+    
+    public Vetor(int capacidade) {
+        this.elementos = new String[capacidade];
+    }
+    
+}
---
+++ estruturadados-algoritmos-java/src/com/regisprojects/estruturadados/vetor/teste/Aula02.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.regisprojects.estruturadados.vetor.teste;
+
+import com.regisprojects.estruturadados.vetor.Vetor;
+
+/**
+ *
+ * @author user
+ */
+public class Aula02 {
+    
+    public static void main(String[] args) {
+        
+        Vetor vetor = new Vetor(5);
+        
+        int i = 0;
+        i++;
+        
+    }
+    
+}
