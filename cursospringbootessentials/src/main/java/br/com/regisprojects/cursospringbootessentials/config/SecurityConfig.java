b60abeea91c55814858fd9a9f490a9cc07faa643
regisPatrick <regis86@hotmail.com>
2020-05-30 01:56:25 -0300

    commit 30-05-2020

---
+++ cursospringbootessentials/src/main/java/br/com/regisprojects/cursospringbootessentials/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.regisprojects.cursospringbootessentials.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ *
+ * @author user
+ */
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .csrf().disable();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth.inMemoryAuthentication()
+                .withUser("regis").password(encoder.encode("123")).roles("USER")
+                .and()
+                .withUser("goku").password(encoder.encode("123")).roles("USER", "ADMIN");
+    }
+
+}
--- cursospringbootessentials/src/main/java/br/com/regisprojects/cursospringbootessentials/endpoint/StudentEndpoint.java
+++ cursospringbootessentials/src/main/java/br/com/regisprojects/cursospringbootessentials/endpoint/StudentEndpoint.java
@@ -20,6 +20,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -82,7 +85,9 @@ public class StudentEndpoint {
 //    }
     
     @GetMapping(path = "/{id}")
-    public ResponseEntity<?> getStudentById(@PathVariable("id") Long id){
+    public ResponseEntity<?> getStudentById(@PathVariable("id") Long id, 
+                                            @AuthenticationPrincipal UserDetails userDetails){
+        System.out.println(userDetails);
         verifyIfStudentExists(id);
         Student student = studentDAO.findStudentById(id);
         return new ResponseEntity<>(student, HttpStatus.OK);
@@ -114,6 +119,7 @@ public class StudentEndpoint {
 //    }
     
     @DeleteMapping(path = "/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> delete(@PathVariable Long id){
         verifyIfStudentExists(id);
         studentDAO.deleteById(id);
