414c1d54c7e6197db297a1319bbcd03018ea3182
regisPatrick <regis86@hotmail.com>
2020-06-07 00:49:17 -0300

    commit 07-06-2020

--- estruturadados-algoritmos-java/src/com/regisprojects/estruturadados/vetor/teste/Aula11.java
+++ estruturadados-algoritmos-java/src/com/regisprojects/estruturadados/vetor/teste/Aula11.java
@@ -5,6 +5,7 @@
  */
 package com.regisprojects.estruturadados.vetor.teste;
 
+import com.regisprojects.estruturadados.vetor.Lista;
 import com.regisprojects.estruturadados.vetor.VetorObjetos;
 
 /**
@@ -15,10 +16,18 @@ public class Aula11 {
     
     public static void main(String[] args) {
         
-        VetorObjetos vetor = new VetorObjetos(2);
+//        VetorObjetos vetor = new VetorObjetos(2);
+//        
+//        vetor.adiciona(1);
+//        vetor.adiciona("Elemento do tipo string");
+//        
+//        System.out.println(vetor);
         
-        vetor.adiciona(1);
-        vetor.adiciona("Elemento do tipo string");
+        Lista<Contato> vetor = new Lista<Contato>(1);
+        
+        Contato c1 = new Contato("Contato 1", "1234-4567", "contato1@email.com");
+        
+        vetor.adiciona(c1);
         
         System.out.println(vetor);
         
