84eaa911a102547c0b81703ae7914f805ad5b4fe
regisPatrick <regis86@hotmail.com>
2020-06-06 23:45:45 -0300

    commit 06-06-2020 2

---
+++ cursospringbootessentials/src/main/java/br/com/regisprojects/cursospringbootessentials/javaclient/JavaClientDAO.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.regisprojects.cursospringbootessentials.javaclient;
+
+import br.com.regisprojects.cursospringbootessentials.model.PageableResponse;
+import br.com.regisprojects.cursospringbootessentials.model.Student;
+import java.util.List;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ *
+ * @author user
+ */
+public class JavaClientDAO {
+
+    private RestTemplate restTemplate = new RestTemplateBuilder()
+            .rootUri("http://localhost:8080/v1/protected/students")
+            .basicAuthentication("pegasus", "123")
+            .build();
+
+    private RestTemplate restTemplateAdmin = new RestTemplateBuilder()
+            .rootUri("http://localhost:8080/v1/admin/students")
+            .basicAuthentication("pegasus", "123")
+            .build();
+
+    public Student findById(long id) {
+//        Student student = 
+        return restTemplate.getForObject("/{id}", Student.class, id);
+//        ResponseEntity<Student> forEntity = restTemplate.getForEntity("/{id}", Student.class, 5);
+    }
+
+    public List<Student> listAll() {
+//        Student[] students = restTemplate.getForObject("/", Student[].class);
+//        System.out.println(Arrays.toString(students));
+//        ResponseEntity<List<Student>> exchange = restTemplate.exchange("/", HttpMethod.GET, null, 
+//                new ParameterizedTypeReference<List<Student>>() {});
+//        System.out.println(exchange.getBody());
+        ResponseEntity<PageableResponse<Student>> exchange = restTemplate.exchange("/", HttpMethod.GET, null, 
+                new ParameterizedTypeReference<PageableResponse<Student>>() {});
+        return exchange.getBody().getContent();
+                                                                                    // ?sort=id,desc&sort=name,asc
+
+    }
+    
+    public Student save(Student student){
+//        System.out.println(exchange);
+//        Student studentPost = new Student();
+//        studentPost.setName("John Wick");
+//        studentPost.setEmail("john@pencil.com");
+        
+        ResponseEntity<Student> exchangePost = restTemplateAdmin.exchange("/", 
+                HttpMethod.POST, new HttpEntity<>(student, createJSONHeader()), Student.class);
+        
+//        Student studentPostForObject = restTemplateAdmin.postForObject("/", studentPost, Student.class);
+//        
+//        ResponseEntity<Student> studentResponseEntity = restTemplateAdmin.postForEntity("/", studentPost, Student.class);
+//        
+//        System.out.println(exchangePost);
+//        
+//        System.out.println(studentPostForObject);
+//        
+//        System.out.println(studentResponseEntity);
+
+        return exchangePost.getBody();
+    }
+    
+    private static HttpHeaders createJSONHeader(){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return headers;
+    }
+
+}
--- cursospringbootessentials/src/main/java/br/com/regisprojects/cursospringbootessentials/javaclient/JavaSpringClientTest.java
+++ cursospringbootessentials/src/main/java/br/com/regisprojects/cursospringbootessentials/javaclient/JavaSpringClientTest.java
@@ -11,7 +11,10 @@ import java.util.Arrays;
 import java.util.List;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
@@ -21,25 +24,18 @@ import org.springframework.web.client.RestTemplate;
  */
 public class JavaSpringClientTest {
     public static void main(String[] args) {
-        RestTemplate restTemplate = new RestTemplateBuilder()
-                .rootUri("http://localhost:8080/v1/protected/students")
-                .basicAuthentication("pegasus", "123")
-                .build();
+      
+        Student studentPost = new Student();
+        studentPost.setName("John Wick");
+        studentPost.setEmail("john@pencil.com");
         
-        Student student = restTemplate.getForObject("/{id}", Student.class, 5);
-        ResponseEntity<Student> forEntity = restTemplate.getForEntity("/{id}", Student.class, 5);
-        System.out.println(student);
-        System.out.println(forEntity.getBody());
-        
-//        Student[] students = restTemplate.getForObject("/", Student[].class);
-//        System.out.println(Arrays.toString(students));
-//        ResponseEntity<List<Student>> exchange = restTemplate.exchange("/", HttpMethod.GET, null, 
-//                new ParameterizedTypeReference<List<Student>>() {});
-//        System.out.println(exchange.getBody());
+        JavaClientDAO dao = new JavaClientDAO();
+//        System.out.println(dao.findById(1));
+//        System.out.println(dao.listAll());
+//        System.out.println(dao.save(studentPost));
 
-        ResponseEntity<PageableResponse<Student>> exchange = restTemplate.exchange("/?sort=id,desc&sort=name,asc", HttpMethod.GET, null, 
-                new ParameterizedTypeReference<PageableResponse<Student>>() {});
-        System.out.println(exchange);
+        List<Student> students = dao.listAll();
+        System.out.println(students);
         
     }
     
