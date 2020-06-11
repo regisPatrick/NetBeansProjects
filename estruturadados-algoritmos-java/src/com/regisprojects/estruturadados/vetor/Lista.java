c0e0e281423a2971b68f199ca71d5a69072b4304
regisPatrick <regis86@hotmail.com>
2020-06-10 23:32:42 -0300

    commit 10-06-2020 2

--- estruturadados-algoritmos-java/src/com/regisprojects/estruturadados/vetor/Lista.java
+++ estruturadados-algoritmos-java/src/com/regisprojects/estruturadados/vetor/Lista.java
@@ -80,6 +80,18 @@ public class Lista<T> {
         return -1;
     }
     
+    public boolean contem(T elemento){
+        
+        /*int pos = busca(elemento);
+        if(pos > -1){
+            return true;
+        }
+        
+        return false;*/
+        
+        return busca(elemento) > -1; // >= 0
+    }
+    
     public void remove(int posicao){
         if(!(posicao >= 0 && posicao < tamanho)){
             throw new IllegalArgumentException("Posição inválida");
