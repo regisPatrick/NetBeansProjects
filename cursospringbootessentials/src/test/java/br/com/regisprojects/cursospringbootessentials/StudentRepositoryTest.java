a790353704ac6cc48e79f630338e71d0c722eb05
regisPatrick <regis86@hotmail.com>
2020-06-13 23:38:02 -0300

    commit 13-06-2020 2

--- cursospringbootessentials/src/test/java/br/com/regisprojects/cursospringbootessentials/StudentRepositoryTest.java
+++ cursospringbootessentials/src/test/java/br/com/regisprojects/cursospringbootessentials/StudentRepositoryTest.java
@@ -8,6 +8,7 @@ package br.com.regisprojects.cursospringbootessentials;
 import br.com.regisprojects.cursospringbootessentials.model.Student;
 import br.com.regisprojects.cursospringbootessentials.repository.StudentRepository;
 import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,6 +34,27 @@ public class StudentRepositoryTest {
         Assertions.assertThat(student.getId()).isNotNull();
         Assertions.assertThat(student.getName()).isEqualTo("Regis");
         Assertions.assertThat(student.getEmail()).isEqualTo("regis@email.com.br");
+    }
+    
+    @Test
+    public void deleteShouldRemoveData(){
+        Student student = new Student("Regis", "regis@email.com.br");
+        this.studentRepository.save(student);
+        studentRepository.delete(student);
+        
+        assertThat(studentRepository.findStudentById(student.getId())).isNull();
+    }
+    
+    @Test
+    public void updateShouldChangeAndPersistData(){
+        Student student = new Student("Regis", "regis@email.com.br");
+        this.studentRepository.save(student);
+        student.setName("Regis222");
+        student.setEmail("regis222@email.com.br");
+        this.studentRepository.save(student);
+        student = this.studentRepository.findStudentById(student.getId());
+        assertThat(student.getName()).isEqualTo("Regis222");
+        assertThat(student.getEmail()).isEqualTo("regis222@email.com.br");
     }
     
 }
