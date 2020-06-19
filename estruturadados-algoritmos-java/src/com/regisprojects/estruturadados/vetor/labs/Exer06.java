c92114e943c6ed48aa0fc36f602a9e62a4ee65c2
regisPatrick <regis86@hotmail.com>
2020-06-19 01:41:22 -0300

    commit 19-06-2020

---
+++ estruturadados-algoritmos-java/src/com/regisprojects/estruturadados/vetor/labs/Exer06.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.regisprojects.estruturadados.vetor.labs;
+
+import com.regisprojects.estruturadados.vetor.Lista;
+import com.regisprojects.estruturadados.vetor.teste.Contato;
+import java.util.Scanner;
+
+/**
+ *
+ * @author user
+ */
+public class Exer06 {
+    
+    public static void main(String[] args) {
+        
+        // Criação das variáveis
+        Scanner scan = new Scanner(System.in);
+        
+        // Criar o vetor com 20 de capacidade
+        Lista<Contato> lista = new Lista<Contato>(20);
+        
+        // Criar e adicionar 30 contatos
+        
+        // criar um menu para que o usuário escolha a opção
+        
+    }
+    
+    private static void criarContatosDinamicamente(int quantidade, Lista<Contato> lista){
+        
+    }
+    
+}
