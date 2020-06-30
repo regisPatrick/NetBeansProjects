5ecca1a08010c30c44786c7e159e0dee7f1aab3f
regisPatrick <regis86@hotmail.com>
2020-06-29 23:28:24 -0300

    commit 29-06-2020 3

---
+++ appevento/src/main/java/br/com/regisprojects/appevento/appevento/AppeventoApplication.java
@@ -0,0 +1,13 @@
+package br.com.regisprojects.appevento.appevento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppeventoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppeventoApplication.class, args);
+	}
+
+}
---
+++ appevento/src/test/java/br/com/regisprojects/appevento/appevento/AppeventoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.regisprojects.appevento.appevento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppeventoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
