0b34d228c7ccdbbf7d3b8ba7be36301f71616474
regisPatrick <regis86@hotmail.com>
2020-06-30 23:46:51 -0300

    commit 30-06-2020

---
+++ appevento/src/main/java/br/com/regisprojects/appevento/appevento/model/Evento.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.regisprojects.appevento.appevento.model;
+
+/**
+ *
+ * @author user
+ */
+public class Evento {
+
+    private String nome;
+    private String local;
+    private String data;
+    private String horario;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+    
+}
