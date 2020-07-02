48e3e32eb104101fbe350af39c4f25c72c69ff72
regisPatrick <regis86@hotmail.com>
2020-07-02 00:53:53 -0300

    commit 02-07-2020

---
+++ appevento/src/main/java/br/com/regisprojects/appevento/appevento/DataConfiguration.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.regisprojects.appevento.appevento;
+
+import javax.sql.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+/**
+ *
+ * @author user
+ */
+@Configuration
+public class DataConfiguration {
+    
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/eventosapp");
+        dataSource.setUsername("root");
+        dataSource.setPassword("root");
+        return dataSource;
+    }
+    
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter(){
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.MYSQL);
+        adapter.setShowSql(true);
+        adapter.setGenerateDdl(true);
+        adapter.setDatabasePlatform("org.hibernate.dialect.MySQL5InnoDBDialect");
+        adapter.setPrepareConnection(true);
+        return adapter;
+    }
+    
+}
--- appevento/src/main/java/br/com/regisprojects/appevento/appevento/model/Evento.java
+++ appevento/src/main/java/br/com/regisprojects/appevento/appevento/model/Evento.java
@@ -5,16 +5,36 @@
  */
 package br.com.regisprojects.appevento.appevento.model;
 
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  *
  * @author user
  */
-public class Evento {
-
+@Entity
+public class Evento implements Serializable{
+    
+    public static final long serialVersionUID = 1L;
+            
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long codigo;
     private String nome;
     private String local;
     private String data;
     private String horario;
+
+    public long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(long codigo) {
+        this.codigo = codigo;
+    }
 
     public String getNome() {
         return nome;
