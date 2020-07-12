4e581283f7926c217f39f6084d3fc1e4e71ed759
regisPatrick <regis86@hotmail.com>
2020-07-12 00:07:12 -0300

    commit 12-07-2020

--- estruturadados-algoritmos-java/src/com/regisprojects/estruturadados/pilha/labs/Exer03.java
+++ estruturadados-algoritmos-java/src/com/regisprojects/estruturadados/pilha/labs/Exer03.java
@@ -24,22 +24,22 @@ public class Exer03 {
         livro1.setIsbn("B01C2XX8Y2");
         
         Livro livro2 = new Livro();
-        livro1.setNome("Learning JavaScript Data Structures and Algorithms");
-        livro1.setAutor("Loiane Groner");
-        livro1.setAnoLancamento(2014);
-        livro1.setIsbn("B00OYTCT02");
+        livro2.setNome("Learning JavaScript Data Structures and Algorithms");
+        livro2.setAutor("Loiane Groner");
+        livro2.setAnoLancamento(2014);
+        livro2.setIsbn("B00OYTCT02");
         
         Livro livro3 = new Livro();
-        livro1.setNome("Mastering Ext JS - Second Edition");
-        livro1.setAutor("Loiane Groner");
-        livro1.setAnoLancamento(2015);
-        livro1.setIsbn("B00U01QQWU");
+        livro3.setNome("Mastering Ext JS - Second Edition");
+        livro3.setAutor("Loiane Groner");
+        livro3.setAnoLancamento(2015);
+        livro3.setIsbn("B00U01QQWU");
         
         Livro livro4 = new Livro();
-        livro1.setNome("JavaScript Regular Expressions");
-        livro1.setAutor("Loiane Groner");
-        livro1.setAnoLancamento(2015);
-        livro1.setIsbn("B00YHBVHG0");
+        livro4.setNome("JavaScript Regular Expressions");
+        livro4.setAutor("Loiane Groner");
+        livro4.setAnoLancamento(2015);
+        livro4.setIsbn("B00YHBVHG0");
         
         System.out.println("Pilha de livros criada, pilha está vazia? " + pilha.estaVazia());
         
