6f25e7b93c48b3c855eaf9b6c24b38de4fa6116e
regisPatrick <regis86@hotmail.com>
2020-07-14 00:16:53 -0300

    commit 14-07-2020

--- estruturadados-algoritmos-java/src/com/regisprojects/estruturadados/pilha/labs/Exer05.java
+++ estruturadados-algoritmos-java/src/com/regisprojects/estruturadados/pilha/labs/Exer05.java
@@ -15,13 +15,34 @@ public class Exer05 {
     
     public static void main(String[] args) {
         
+        imprimeResultado("ADA");
+        imprimeResultado("ABCD");
+        imprimeResultado("ABCCBA");
+        imprimeResultado("Maria");
         
-        
+    }
+    
+    public static void imprimeResultado(String palavra){
+        System.out.println(palavra + " é palíndromo? " + testaPalindromo(palavra));
     }
     
     public static boolean testaPalindromo(String palavra){
         
         Pilha<Character> pilha = new Pilha<>();
+        
+        for (int i = 0; i < palavra.length(); i++){
+            pilha.empilha(palavra.charAt(i));
+        }
+        
+        String palavraInversa = "";
+        
+        while(!pilha.estaVazia()){
+            palavraInversa += pilha.desempilha();
+        }
+        
+        if(palavraInversa.equalsIgnoreCase(palavra)){
+            return true;
+        }
         
         return false;
     }
