6cb1479f57cacf48df29ada40e506d28dc70619a
regisPatrick <regis86@hotmail.com>
2020-07-13 23:51:37 -0300

    commit 13-07-2020

---
+++ estruturadados-algoritmos-java/src/com/regisprojects/estruturadados/pilha/labs/Exer05.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.regisprojects.estruturadados.pilha.labs;
+
+/**
+ *
+ * @author user
+ */
+public class Exer05 {
+    
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+}
