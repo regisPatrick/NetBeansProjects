13512bc4950e0ecb9d8475722d571b9060c5c899
regisPatrick <regis86@hotmail.com>
2020-07-15 00:43:26 -0300

    commit 15-07-2020

---
+++ estruturadados-algoritmos-java/src/com/regisprojects/estruturadados/pilha/labs/Exer06.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.regisprojects.estruturadados.pilha.labs;
+
+import com.regisprojects.estruturadados.pilha.Pilha;
+
+/**
+ *
+ * @author user
+ */
+public class Exer06 {
+    
+    public static void main(String[] args) {
+        
+        imprimeResultado("A + B");
+        imprimeResultado("A + B + (C - D)");
+        imprimeResultado("{[()]}[](){()}");
+        imprimeResultado("{[(]}[](){()}");
+        imprimeResultado("A + B + C - D)");
+        
+    }
+    
+    public static void imprimeResultado(String expressao){
+        System.out.println(expressao + " está balanceado? " + verificaSimbolosBalanceados(expressao));
+    }
+    
+    final static String ABRE = "([{";
+    final static String FECHA = ")]}";
+    
+    public static boolean verificaSimbolosBalanceados(String expressao){
+        
+        Pilha<Character> pilha = new Pilha<>();
+        int index =0;
+        char simbolo, topo;
+        
+        while(index < expressao.length()){
+            simbolo = expressao.charAt(index);
+            
+            if (ABRE.indexOf(simbolo) > -1){
+                pilha.empilha(simbolo);
+            } else if (FECHA.indexOf(simbolo) > -1){
+                
+                if(pilha.estaVazia()){
+                    return false;
+                } else {
+                    topo = pilha.desempilha();
+                    
+                    if(ABRE.indexOf(topo) != FECHA.indexOf(simbolo)){
+                        return false;
+                    }
+                }
+                
+            }
+            
+            index++;
+        }
+        
+        return true;
+    }
+    
+}
