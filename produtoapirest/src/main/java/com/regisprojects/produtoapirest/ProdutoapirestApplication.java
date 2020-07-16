4172be5146ca781fe32835b49db71a0e9298dfe4
regisPatrick <regis86@hotmail.com>
2020-07-16 00:10:38 -0300

    commit 16-07-2020

---
+++ produtoapirest/src/main/java/com/regisprojects/produtoapirest/ProdutoapirestApplication.java
@@ -0,0 +1,13 @@
+package com.regisprojects.produtoapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutoapirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutoapirestApplication.class, args);
+	}
+
+}
---
+++ produtoapirest/src/test/java/com/regisprojects/produtoapirest/ProdutoapirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.regisprojects.produtoapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutoapirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
