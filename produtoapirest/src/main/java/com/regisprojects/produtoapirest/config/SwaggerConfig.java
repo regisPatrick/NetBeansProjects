100ae524cde0aae420af6ad758b203c2cd50b55e
regisPatrick <regis86@hotmail.com>
2020-07-20 23:59:49 -0300

    commit 20-07-2020

---
+++ produtoapirest/src/main/java/com/regisprojects/produtoapirest/config/SwaggerConfig.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.regisprojects.produtoapirest.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ *
+ * @author user
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    
+    @Bean
+    public Docket productApi(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.regisprojects.produtoapirest"))
+                .paths(regex("/api.*"))
+                .build()
+                .apiInfo(metaInfo());
+    }
+    
+    private ApiInfo metaInfo(){
+        ApiInfo apiInfo = new ApiInfo(
+                "Produtos API REST",
+                "API REST de cadastro de produtos.",
+                "1.0",
+                "Terms of Service",
+                new Contact("Regis Saletti", "https://www.youtube.com",
+                    "regis@email.com"),
+                "Apache License Version 2.0",
+                "https://www.apache.org/licesen.html"
+        )
+    }
+    
+}
