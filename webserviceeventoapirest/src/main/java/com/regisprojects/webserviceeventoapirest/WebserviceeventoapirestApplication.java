fb82202d5932755c7a4dadf52a0a52ef9b75377f
regisPatrick <regis86@hotmail.com>
2020-07-23 02:14:29 -0300

    commit 23-07-2020

---
+++ webserviceeventoapirest/src/main/java/com/regisprojects/webserviceeventoapirest/WebserviceeventoapirestApplication.java
@@ -0,0 +1,13 @@
+package com.regisprojects.webserviceeventoapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceeventoapirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserviceeventoapirestApplication.class, args);
+	}
+
+}
---
+++ webserviceeventoapirest/src/test/java/com/regisprojects/webserviceeventoapirest/WebserviceeventoapirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.regisprojects.webserviceeventoapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceeventoapirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
