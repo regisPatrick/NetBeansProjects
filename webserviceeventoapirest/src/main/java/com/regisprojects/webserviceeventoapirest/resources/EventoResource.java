a6674d2ba02f4b25ffc448e539cb94f2527251bd
regisPatrick <regis86@hotmail.com>
2020-07-24 23:42:14 -0300

    commit 24-07-2020

--- webserviceeventoapirest/src/main/java/com/regisprojects/webserviceeventoapirest/resources/EventoResource.java
+++ webserviceeventoapirest/src/main/java/com/regisprojects/webserviceeventoapirest/resources/EventoResource.java
@@ -7,8 +7,12 @@ package com.regisprojects.webserviceeventoapirest.resources;
 
 import com.regisprojects.webserviceeventoapirest.models.Evento;
 import com.regisprojects.webserviceeventoapirest.repository.EventoRepository;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +32,17 @@ public class EventoResource {
     public @ResponseBody Iterable<Evento> listaEventos(){
         Iterable<Evento> listaEventos = er.findAll();
         return listaEventos;
+    }
+    
+    @PostMapping()
+    public Evento cadastraEvento(@RequestBody @Valid Evento evento){
+        return er.save(evento);
+    }
+    
+    @DeleteMapping
+    public Evento deletaEvento(@RequestBody Evento evento){
+        er.delete(evento);
+        return evento;
     }
     
 }
