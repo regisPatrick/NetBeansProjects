dc89a32189817968bc97c5923b7c513109140927
regisPatrick <regis86@hotmail.com>
2020-07-26 23:43:35 -0300

    commit 26-07-2020 2

--- webserviceeventoapirest/src/main/java/com/regisprojects/webserviceeventoapirest/SwaggerConfig.java
+++ webserviceeventoapirest/src/main/java/com/regisprojects/webserviceeventoapirest/SwaggerConfig.java
@@ -5,12 +5,14 @@
  */
 package com.regisprojects.webserviceeventoapirest;
 
+import java.util.ArrayList;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import static springfox.documentation.builders.PathSelectors.regex;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -43,7 +45,7 @@ public class SwaggerConfig {
                 new Contact("Regis Patrick", "https://www.youtube.com/",
                         "regis@email.com"),
                 "Apache License Version 2.0",
-                "https://www.apache.org/licesen.html", null
+                "https://www.apache.org/licesen.html", new ArrayList<VendorExtension>()
         );
         return apiInfo;
     }
