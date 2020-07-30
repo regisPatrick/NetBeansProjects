81b3c166f2e2378c07030839a480ce9fd903108e
regisPatrick <regis86@hotmail.com>
2020-07-30 00:22:35 -0300

    commit -m 30-07-2020

---
+++ springwebfluxapi/src/main/java/com/regisprojects/springwebfluxapi/SpringwebfluxapiApplication.java
@@ -0,0 +1,14 @@
+package com.regisprojects.springwebfluxapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebfluxapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebfluxapiApplication.class, args);
+                System.out.println("Olá mundo com webflux!!!");
+	}
+
+}
---
+++ springwebfluxapi/src/test/java/com/regisprojects/springwebfluxapi/SpringwebfluxapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.regisprojects.springwebfluxapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebfluxapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
