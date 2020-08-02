605cbc595a4c3eed4d3037b0babfd8dcd0ddcaa1
regisPatrick <regis86@hotmail.com>
2020-08-02 02:07:28 -0300

    commit 02-08-2020

---
+++ springwebfluxapi/src/main/java/com/regisprojects/springwebfluxapi/services/PlaylistServiceImpl.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.regisprojects.springwebfluxapi.services;
+
+import com.regisprojects.springwebfluxapi.document.Playlist;
+import com.regisprojects.springwebfluxapi.repository.PlaylistRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+/**
+ *
+ * @author user
+ */
+@Service
+public class PlaylistServiceImpl implements PlaylistService{
+
+    @Autowired
+    PlaylistRepository pr;
+    
+    @Override
+    public Flux<Playlist> findAll() {
+        return pr.findAll();
+    }
+
+    @Override
+    public Mono<Playlist> findById(String id) {
+        return pr.findById(id);
+    }
+
+    @Override
+    public Mono<Playlist> save(Playlist playlist) {
+        return pr.save(playlist);
+    }
+    
+}
