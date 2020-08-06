b06958cace59bb2fb26d0d3e42581e3f8c918620
regisPatrick <regis86@hotmail.com>
2020-08-05 23:57:15 -0300

    commit 05-08-2020

---
+++ springbootblog/src/test/java/com/regisprojects/springbootblog/SpringbootblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.regisprojects.springbootblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
