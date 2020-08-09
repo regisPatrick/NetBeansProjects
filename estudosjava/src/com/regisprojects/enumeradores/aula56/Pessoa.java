b193fc63e2a1d601fcfc33a47b4755d9019cd160
regisPatrick <regis86@hotmail.com>
2020-08-09 03:05:23 -0300

    commit 09-08-2020 2

---
+++ estudosjava/src/com/regisprojects/enumeradores/aula56/Pessoa.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.regisprojects.enumeradores.aula56;
+
+/**
+ *
+ * @author user
+ */
+public class Pessoa {
+    
+    private TipoDocumento tipoDocumento;
+    private String numeroDocumento;
+
+    public Pessoa() {
+    }
+
+    public Pessoa(TipoDocumento tipoDocumento, String numeroDocumento) {
+        this.tipoDocumento = tipoDocumento;
+        this.numeroDocumento = numeroDocumento;
+    }
+
+    public TipoDocumento getTipoDocumento() {
+        return tipoDocumento;
+    }
+
+    public void setTipoDocumento(TipoDocumento tipoDocumento) {
+        this.tipoDocumento = tipoDocumento;
+    }
+
+    public String getNumeroDocumento() {
+        return numeroDocumento;
+    }
+
+    public void setNumeroDocumento(String numeroDocumento) {
+        this.numeroDocumento = numeroDocumento;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" + "tipoDocumento=" + tipoDocumento + ", numeroDocumento=" + numeroDocumento + '}';
+    }
+    
+}
