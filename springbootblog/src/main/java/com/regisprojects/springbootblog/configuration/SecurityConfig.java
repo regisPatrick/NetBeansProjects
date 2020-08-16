ef6e7f28f5df7b49a59f9a13a86b0ec6ac8443ec
regisPatrick <regis86@hotmail.com>
2020-08-16 19:11:51 -0300

    commit 16-08-2020

---
+++ springbootblog/src/main/java/com/regisprojects/springbootblog/configuration/SecurityConfig.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.regisprojects.springbootblog.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+/**
+ *
+ * @author user
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+    
+    private static final String[] AUTH_LIST = {
+        "/",
+        "/posts",
+        "/posts/{id}"
+    };
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.csrf().disable().authorizeRequests()
+                .antMatchers(AUTH_LIST).permitAll()
+                .anyRequest().authenticated()
+                .and().formLogin().permitAll()
+                .and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+    }
+    
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("regis").password("{noop}123").roles("ADMIN");
+    }
+    
+    @Override
+    public void configure(WebSecurity web) throws Exception{
+        web.ignoring().antMatchers("/bootstrap/**");
+    }
+    
+}
