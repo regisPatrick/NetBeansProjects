eb12a30d64f02f1e839fdd28c26ad9f07ba079a3
regisPatrick <regis86@hotmail.com>
2020-08-22 15:44:23 -0300

    commit 22-08-2020

---
+++ project-reactor-essentials/src/test/java/com/regisprojects/projectreactoressentials/ProjectReactorEssentialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.regisprojects.projectreactoressentials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectReactorEssentialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ project-reactor-essentials/src/test/java/com/regisprojects/projectreactoressentials/test/MonoTest.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.regisprojects.projectreactoressentials.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author user
+ */
+@Slf4j
+public class MonoTest {
+    
+    @Test
+    public void test(){
+        log.info("Everything working as intendend");
+    }
+    
+}
