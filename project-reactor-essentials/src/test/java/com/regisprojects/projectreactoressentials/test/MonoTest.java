ca2d5a691e9b64b42f3f7e1608dbe4bb1c90464e
regisPatrick <regis86@hotmail.com>
2020-08-25 01:30:42 -0300

    commit 25-08-2020

--- project-reactor-essentials/src/test/java/com/regisprojects/projectreactoressentials/test/MonoTest.java
+++ project-reactor-essentials/src/test/java/com/regisprojects/projectreactoressentials/test/MonoTest.java
@@ -7,6 +7,8 @@ package com.regisprojects.projectreactoressentials.test;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 /**
  *
@@ -15,9 +17,24 @@ import org.junit.jupiter.api.Test;
 @Slf4j
 public class MonoTest {
     
+//    @Test
+//    public void test(){
+//        log.info("Everything working as intendend");
+//    }
+    
     @Test
-    public void test(){
-        log.info("Everything working as intendend");
+    public void monoSubscriber(){
+        String name = "regis";
+        Mono<String> mono = Mono.just(name)
+                .log();
+        
+        mono.subscribe();
+        log.info("---------------------------------------");
+        StepVerifier.create(mono)
+                .expectNext(name)
+                .verifyComplete();
+//        log.info("Mono {}", mono);
+//        log.info("Everything working as intendend");
     }
     
 }
