5262cd4368e502aba8adc72a5b83d4fcc97d6072
regisPatrick <regis86@hotmail.com>
2020-09-01 19:43:04 -0300

    commit 01-09-2020

--- spring-webflux-essentials/src/main/java/com/regisprojects/springwebfluxessentials/exception/GlobalExceptionHandler.java
+++ spring-webflux-essentials/src/main/java/com/regisprojects/springwebfluxessentials/exception/GlobalExceptionHandler.java
@@ -11,12 +11,14 @@ import java.util.OptionalInt;
 import org.springframework.boot.autoconfigure.web.ResourceProperties;
 import org.springframework.boot.autoconfigure.web.reactive.error.AbstractErrorWebExceptionHandler;
 import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.error.ErrorAttributeOptions.Include;
 import org.springframework.boot.web.reactive.error.ErrorAttributes;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.MediaType;
 import org.springframework.http.codec.ServerCodecConfigurer;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
@@ -47,13 +49,20 @@ public class GlobalExceptionHandler extends AbstractErrorWebExceptionHandler{
     }
     
     private Mono<ServerResponse> formatErrorResponse(ServerRequest request){
+        String query = request.uri().getQuery();
+        ErrorAttributeOptions errorAttributeOptions = isTraceEnabled(query) ? ErrorAttributeOptions.of(Include.STACK_TRACE) : ErrorAttributeOptions.defaults();
 //        Map<String, Object> errorAttributesMap = getErrorAttributes(request, ErrorAttributeOptions.defaults());
-        Map<String, Object> errorAttributesMap = getErrorAttributes(request, ErrorAttributeOptions.of(ErrorAttributeOptions.Include.STACK_TRACE));
+//        Map<String, Object> errorAttributesMap = getErrorAttributes(request, ErrorAttributeOptions.of(ErrorAttributeOptions.Include.STACK_TRACE));
+        Map<String, Object> errorAttributesMap = getErrorAttributes(request, errorAttributeOptions);
         int status = (int) Optional.ofNullable(errorAttributesMap.get("status")).orElse(500);
         
         return ServerResponse.status(status)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(BodyInserters.fromValue(errorAttributesMap));
+    }
+ 
+    private boolean isTraceEnabled(String query){
+        return !StringUtils.isEmpty(query) && query.contains("trace=true");
     }
     
 }
