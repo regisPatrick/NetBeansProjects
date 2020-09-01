f1306d8e1bd8f8ee1b2e652fe824642d0856cd0b
regisPatrick <regis86@hotmail.com>
2020-09-01 00:36:13 -0300

    commit 31-08-2020 2

--- spring-webflux-essentials/src/main/java/com/regisprojects/springwebfluxessentials/exception/GlobalExceptionHandler.java
+++ spring-webflux-essentials/src/main/java/com/regisprojects/springwebfluxessentials/exception/GlobalExceptionHandler.java
@@ -5,14 +5,25 @@
  */
 package com.regisprojects.springwebfluxessentials.exception;
 
+import java.util.Map;
+import java.util.Optional;
+import java.util.OptionalInt;
 import org.springframework.boot.autoconfigure.web.ResourceProperties;
 import org.springframework.boot.autoconfigure.web.reactive.error.AbstractErrorWebExceptionHandler;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.boot.web.reactive.error.ErrorAttributes;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerCodecConfigurer;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
 
 /**
  *
@@ -24,13 +35,25 @@ public class GlobalExceptionHandler extends AbstractErrorWebExceptionHandler{
 
     public GlobalExceptionHandler(ErrorAttributes errorAttributes, 
             ResourceProperties resourceProperties, 
-            ApplicationContext applicationContext) {
+            ApplicationContext applicationContext,
+            ServerCodecConfigurer codecConfigurer) {
         super(errorAttributes, resourceProperties, applicationContext);
+        this.setMessageWriters(codecConfigurer.getWriters());
     }
 
     @Override
     protected RouterFunction<ServerResponse> getRoutingFunction(ErrorAttributes errorAttributes) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return RouterFunctions.route(RequestPredicates.all(), this::formatErrorResponse);
+    }
+    
+    private Mono<ServerResponse> formatErrorResponse(ServerRequest request){
+//        Map<String, Object> errorAttributesMap = getErrorAttributes(request, ErrorAttributeOptions.defaults());
+        Map<String, Object> errorAttributesMap = getErrorAttributes(request, ErrorAttributeOptions.of(ErrorAttributeOptions.Include.STACK_TRACE));
+        int status = (int) Optional.ofNullable(errorAttributesMap.get("status")).orElse(500);
+        
+        return ServerResponse.status(status)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(errorAttributesMap));
     }
     
 }
--- spring-webflux-essentials/src/main/java/com/regisprojects/springwebfluxessentials/service/AnimeService.java
+++ spring-webflux-essentials/src/main/java/com/regisprojects/springwebfluxessentials/service/AnimeService.java
@@ -36,8 +36,8 @@ public class AnimeService {
     
     public Mono<Anime> findById(int id){
         return animeRepository.findById(id)
-                .switchIfEmpty(monoResponseStatusNotFoundException())
-                .log();
+                .switchIfEmpty(monoResponseStatusNotFoundException());
+//                .log();
     }
     
     public <T> Mono<T> monoResponseStatusNotFoundException(){
