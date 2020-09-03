0fbff2b1c3d3df7afccc05d49ff466a78057cd2b
regisPatrick <regis86@hotmail.com>
2020-09-03 00:21:50 -0300

    commit 03-09-2020

--- spring-webflux-essentials/src/main/java/com/regisprojects/springwebfluxessentials/exception/CustomAttributes.java
+++ spring-webflux-essentials/src/main/java/com/regisprojects/springwebfluxessentials/exception/CustomAttributes.java
@@ -5,8 +5,12 @@
  */
 package com.regisprojects.springwebfluxessentials.exception;
 
+import java.util.Map;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.boot.web.reactive.error.DefaultErrorAttributes;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.server.ResponseStatusException;
 
 /**
  *
@@ -14,5 +18,17 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class CustomAttributes extends DefaultErrorAttributes{
-    
+
+    @Override
+    public Map<String, Object> getErrorAttributes(ServerRequest request, ErrorAttributeOptions options) {
+        Map<String, Object> errorAttributesMap = super.getErrorAttributes(request, options);
+        Throwable throwable = getError(request);
+        if(throwable instanceof ResponseStatusException){
+            ResponseStatusException ex = (ResponseStatusException)throwable;
+            errorAttributesMap.put("message", ex.getMessage());
+            errorAttributesMap.put("developerMessage", "A ResponseStatusException happened");
+        }
+        return errorAttributesMap;
+    }
+
 }
