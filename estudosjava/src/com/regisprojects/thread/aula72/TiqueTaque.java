529c974ebd97f9e3521cf854b1dd503a75408a7c
regisPatrick <regis86@hotmail.com>
2020-09-07 00:11:52 -0300

    commit 07-09-2020

---
+++ estudosjava/src/com/regisprojects/thread/aula72/TiqueTaque.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.regisprojects.thread.aula72;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author user
+ */
+public class TiqueTaque {
+    
+    boolean tique;
+    
+    synchronized void tique(boolean estaExecutando){
+        
+        if(!estaExecutando){
+            tique = true;
+            notify();
+            return;
+        }
+        
+        System.out.print("Tique ");
+        
+        tique = true;
+        
+        notify();
+        
+        try {
+            while (tique) {
+                wait();
+            }
+        } catch (InterruptedException ex) {
+            Logger.getLogger(TiqueTaque.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    synchronized void taque(boolean estaExecutando){
+        
+        if(!estaExecutando){
+            tique = false;
+            notify();
+            return;
+        }
+        
+        System.out.println("Taque");
+        
+        tique = false;
+        
+        notify();
+        
+        try {
+            while (!tique) {
+                wait();
+            }
+        } catch (InterruptedException ex) {
+            Logger.getLogger(TiqueTaque.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
