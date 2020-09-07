314e32eb8a0ca3be1ea41aca3afba3f423b1263f
regisPatrick <regis86@hotmail.com>
2020-09-06 22:44:06 -0300

    commit 06-09-2020 2

---
+++ java-8/src/lambdas/teste/LambdaTeste.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lambdas.teste;
+
+/**
+ *
+ * @author user
+ */
+public class LambdaTeste {
+    
+    public static void main(String[] args) {
+        // Anônima
+        // Função
+        // Pode ser passada por argumento para algum método ou guardada em uma variável
+        // Concisa
+        // (Carro carro) -> carro.getCor().equals("verde");
+        // (String s) -> s.length()
+        // (int a, int b) -> {System.out.println(a + b)};
+        
+        
+    }
+    
+}
