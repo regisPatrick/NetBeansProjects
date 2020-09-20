3eaa772b47c68ab980a050b4735d715fded7753c
regisPatrick <regis86@hotmail.com>
2020-09-20 01:15:51 -0300

    commit 20-09-2020

--- java-8/src/optional/teste/OptionalTest1.java
+++ java-8/src/optional/teste/OptionalTest1.java
@@ -20,11 +20,21 @@ public class OptionalTest1 {
         OptionalTest1 ot = new OptionalTest1();
         Optional<String> optional1 = Optional.of("Regis");
         Optional<String> optional2 = Optional.empty();
+        Optional<String> optional3 = Optional.ofNullable(ot.nome);
+        // Imprimindo
         System.out.println(optional1);
         System.out.println(optional2);
 //        if(ot.nome != null){
 //            System.out.println(ot.nome.toUpperCase());
 //        }
+        System.out.println(optional3);
+        // Buscando valores
+        System.out.println(optional1.orElse("vazio"));
+        if(optional2.isPresent()){
+            System.out.println(optional2.get());
+        }
+        System.out.println(optional2.orElse("vazio"));
+        System.out.println(optional3.orElse("vazio"));
     }
     
 }
