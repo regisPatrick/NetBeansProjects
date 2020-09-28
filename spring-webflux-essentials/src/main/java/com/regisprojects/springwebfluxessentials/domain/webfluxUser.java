ed9633a8f6d64f88791d21c2bcada0c4a0b4661f
regisPatrick <regis86@hotmail.com>
2020-09-28 01:54:00 -0300

    commit 28-09-2020

---
+++ spring-webflux-essentials/src/main/java/com/regisprojects/springwebfluxessentials/domain/webfluxUser.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.regisprojects.springwebfluxessentials.domain;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+/**
+ *
+ * @author user
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table("webflux_User")
+public class webfluxUser implements UserDetails{
+
+    @Id
+    private Integer id;
+    private String name;
+    private String username;
+    private String password;
+    private String authorities; // ROLE_USER, ROLE_ADMIN
+    
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.stream(authorities.split(","))
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    
+    
+}
