9f60c3d6d6f723084a6aa5ed88662d599f348f2c
regisPatrick <regis86@hotmail.com>
2020-10-03 02:19:17 -0300

    commit 03-10-2020

--- java-8/src/streams/StreamTest.java
+++ java-8/src/streams/StreamTest.java
@@ -5,12 +5,42 @@
  */
 package streams;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  *
  * @author user
  */
 public class StreamTest {
     // Pegar os três primeiros nomes das pessoas com menos de 25 anos.
+    // Ordenados pelo nome.
     
+    public static void main(String[] args) {
+        
+        List<Pessoa> pessoas = Pessoa.bancoDePessoas();
+        Collections.sort(pessoas, (o1, o2) -> o1.getNome().compareTo(o2.getNome()));
+        List<String> nomes = new ArrayList<>();
+        for(Pessoa pessoa : pessoas){
+            if(pessoa.getIdade() < 25){
+                nomes.add(pessoa.getNome());
+                if(nomes.size() >= 3)
+                    break;
+            }
+        }
+        System.out.println(nomes);
+        
+        List<String> nomes2 = pessoas
+                .stream()
+                .filter(p -> p.getIdade() < 25)
+                .sorted(Comparator.comparing(Pessoa::getNome))
+                .limit(3)
+                .map(Pessoa::getNome)
+                .collect(Collectors.toList());
+        System.out.println(nomes2);
+    }
     
 }
