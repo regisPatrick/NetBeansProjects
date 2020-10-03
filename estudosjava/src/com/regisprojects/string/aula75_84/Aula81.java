b5d0c2f9d09758562a931661dd5fe4c6f3904030
regisPatrick <regis86@hotmail.com>
2020-10-03 03:03:23 -0300

    commit 03-10-2020 2

---
+++ estudosjava/src/com/regisprojects/string/aula75_84/Aula81.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.regisprojects.string.aula75_84;
+
+/**
+ *
+ * @author user
+ */
+public class Aula81 {
+    
+    public static void main(String[] args) {
+        
+        String teste = "Teste";
+        String testeMinusc = teste.toLowerCase();
+        String testeMaiusc = teste.toUpperCase();
+        
+        System.out.println(teste.toLowerCase());
+        System.out.println(testeMinusc);
+        System.out.println(testeMaiusc);
+        
+        // if (teste.toLowerCase().equals(teste.toLowerCase()))
+        
+    }
+    
+}
