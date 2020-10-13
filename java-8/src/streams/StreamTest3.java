2d4ef8665dbbf9a93a99650fc9de705831159722
regisPatrick <regis86@hotmail.com>
2020-10-12 23:58:44 -0300

    commit 12-10-2020 4

---
+++ java-8/src/streams/StreamTest3.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package streams;
+
+import java.util.List;
+
+/**
+ *
+ * @author user
+ */
+public class StreamTest3 {
+    
+    public static void main(String[] args) {
+        
+        List<Pessoa> pessoas = Pessoa.bancoDePessoas();
+        
+    }
+    
+}
