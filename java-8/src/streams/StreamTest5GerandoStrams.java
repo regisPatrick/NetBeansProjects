80ca1627904b785d6e6441734fc7f47ab02c2933
regisPatrick <regis86@hotmail.com>
2020-10-28 23:52:26 -0300

    commit 28-10-2020

---
+++ java-8/src/streams/StreamTest5GerandoStrams.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package streams;
+
+import java.util.stream.IntStream;
+
+/**
+ *
+ * @author user
+ */
+public class StreamTest5GerandoStrams {
+    
+    public static void main(String[] args) {
+        
+        IntStream.rangeClosed(1, 100);
+        
+    }
+    
+}
