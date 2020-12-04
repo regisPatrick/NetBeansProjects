c27d7c541065d8085381d051f39e3b1dcef6d12c
regisPatrick <regis86@hotmail.com>
2020-12-03 23:57:27 -0300

    commit 03-12-2020 2

--- api-rest-produtos/src/main/java/com/produtos/apirestprodutos/resources/ProdutoResource.java
+++ api-rest-produtos/src/main/java/com/produtos/apirestprodutos/resources/ProdutoResource.java
@@ -11,6 +11,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,6 +36,11 @@ public class ProdutoResource {
     @GetMapping("/produto/{id}")
     public Produto listaProdutoUnico(@PathVariable(value="id") long id){
         return produtoRepository.findById(id);
+    }
+    
+    @PostMapping("/produto")
+    public Produto salvaProduto(@RequestBody Produto produto){
+        return null;
     }
     
 }
