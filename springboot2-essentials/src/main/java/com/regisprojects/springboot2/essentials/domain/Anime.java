3d5bf05b388b70d2489697f7a243c51a027e72c2
regisPatrick <regis86@hotmail.com>
2021-02-01 00:47:10 -0300

    commit 01-02-2021

---
+++ springboot2-essentials/src/main/java/com/regisprojects/springboot2/essentials/domain/Anime.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.regisprojects.springboot2.essentials.domain;
+
+/**
+ *
+ * @author user
+ */
+public class Anime {
+    
+    private String name;
+
+    public Anime(String name) {
+        this.name = name;
+    }
+
+    public Anime() {
+    }
+    
+}
