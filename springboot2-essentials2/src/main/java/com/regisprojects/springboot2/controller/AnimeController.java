35141d680ff47b697af6cde30bb91140720a386f
regisPatrick <regis86@hotmail.com>
2021-02-28 14:33:33 -0300

    commit 28-02-2021 2

---
+++ springboot2-essentials2/src/main/java/com/regisprojects/springboot2/controller/AnimeController.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.regisprojects.springboot2.controller;
+
+
+import com.regisprojects.springboot2.domain.Anime;
+import com.regisprojects.springboot2.util.DateUtil;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author user
+ */
+@RestController
+@RequestMapping("anime")
+@Log4j2
+@RequiredArgsConstructor
+public class AnimeController {
+   
+    private final DateUtil dateUtil;
+
+    @GetMapping(path = "list")
+    public List<Anime> list(){
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        return List.of(new Anime("Boku no Hero"), new Anime("Berserk"));
+    }
+    
+}
---
+++ springboot2-essentials2/src/main/java/com/regisprojects/springboot2/domain/Anime.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.regisprojects.springboot2.domain;
+
+/**
+ *
+ * @author user
+ */
+public class Anime {
+    
+    private String name;
+
+    public Anime(String name) {
+        this.name = name;
+    }
+
+    public Anime() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
