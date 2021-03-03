a45fbc2e94e972a7b94f7164df86dfd1dde1088b
regisPatrick <regis86@hotmail.com>
2021-03-03 01:57:55 -0300

    commit 03-03-2021 3

--- springboot2-essentials2/src/main/java/com/regisprojects/springboot2/service/AnimeService.java
+++ springboot2-essentials2/src/main/java/com/regisprojects/springboot2/service/AnimeService.java
@@ -16,6 +16,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class AnimeService {
     
+    //private final AnimeRepository animeRepository;
+    
     public List<Anime> listAll(){
         return  List.of(new Anime(1L, "Boku no Hero"), new Anime(2L, "Berserk"));
     }
