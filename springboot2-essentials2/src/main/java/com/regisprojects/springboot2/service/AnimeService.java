b628b809db6d08087dbdf962ab585887fa086ff7
regisPatrick <regis86@hotmail.com>
2021-03-07 23:21:47 -0300

    commit 07-03-2021

--- springboot2-essentials2/src/main/java/com/regisprojects/springboot2/service/AnimeService.java
+++ springboot2-essentials2/src/main/java/com/regisprojects/springboot2/service/AnimeService.java
@@ -7,7 +7,9 @@ package com.regisprojects.springboot2.service;
 
 import com.regisprojects.springboot2.domain.Anime;
 import java.util.List;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 /**
  *
@@ -16,10 +18,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class AnimeService {
     
+    private List<Anime> animes = List.of(new Anime(1L, "Boku no Hero"), new Anime(2L, "Berserk"));
+    
     //private final AnimeRepository animeRepository;
     
     public List<Anime> listAll(){
-        return  List.of(new Anime(1L, "Boku no Hero"), new Anime(2L, "Berserk"));
+        return animes;
+    }
+    
+    public Anime findById(long id){
+        return animes.stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not Found"));
     }
     
 }
