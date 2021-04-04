733253ef82b1f427a59d270a129c5a425abaaf26
regisPatrick <regis86@hotmail.com>
2021-04-04 16:34:51 -0300

    commit 04-04-2021

--- springboot2-essentials2/src/main/java/com/regisprojects/springboot2/controller/AnimeController.java
+++ springboot2-essentials2/src/main/java/com/regisprojects/springboot2/controller/AnimeController.java
@@ -19,6 +19,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -44,6 +46,11 @@ public class AnimeController {
     @GetMapping(path = "/{id}")
     public ResponseEntity<Anime> findById(@PathVariable long id){
         return ResponseEntity.ok(animeService.findById(id));
+    }
+    
+    @PostMapping
+    public ResponseEntity<Anime> save(@RequestBody Anime anime){
+        animeService.save(anime);
     }
     
 }
